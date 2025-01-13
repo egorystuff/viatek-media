17b50a6b2a7460c5c30b819c79e15410c08d5b30
Yahor Yankavets <mr.yankovec@gmail.com>
2025-01-13 23:41:06 +0300

    some components have been updated

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,6 +3,10 @@ const elements = document.querySelectorAll(".services__block-item");
 elements.forEach((element) => {
   const color = element.getAttribute("data-color") || "#112630";
   const size = element.hasAttribute("data-size") ? "1000px" : "600px";
+  const style = element.hasAttribute("data-style") ? "second" : "base";
+
   element.style.setProperty("--color", color);
   element.style.setProperty("--size", size);
+
+  element.classList.add(style);
 });
