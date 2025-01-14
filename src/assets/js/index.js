86745952683e34b310e85109097a897ad899ec07
Yahor Yankavets <mr.yankovec@gmail.com>
2025-01-14 23:50:41 +0300

    update

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,6 @@
+//  services__block animation
+//-------------------------------------------------------------------------------------
+
 const elements = document.querySelectorAll(".services__block-item");
 
 elements.forEach((element) => {
@@ -9,4 +12,27 @@ elements.forEach((element) => {
   element.style.setProperty("--size", size);
 
   element.classList.add(style);
+});
+
+//  price__block add active
+//-------------------------------------------------------------------------------------
+
+const priceBlockItems = document.querySelectorAll(".price__block-item");
+
+priceBlockItems.forEach((item) => {
+  const topBlock = item.querySelector(".price__block-item-top");
+  const detailsBlock = item.querySelector(".price__block-item-details");
+
+  topBlock.addEventListener("click", () => {
+    detailsBlock.classList.toggle("active");
+  });
+});
+
+priceBlockItems.forEach((item) => {
+  const topBlock = item.querySelector(".price__block-item-top");
+  const icon = item.querySelector(".item-icon");
+
+  topBlock.addEventListener("click", () => {
+    icon.classList.toggle("active");
+  });
 });
